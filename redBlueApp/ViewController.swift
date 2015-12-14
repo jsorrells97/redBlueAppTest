db78ffe1fef5dacf343bafe5f6d60be49bc84496
Justin Sorrells <bestdfs@hotmail.com>
2015-12-14 16:33:47 -0500

    Commiting initial redBlueApp

--- redBlueApp/ViewController.swift
+++ redBlueApp/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var blueColor: UIImageView!
+    @IBOutlet weak var redColor: UIImageView!
+    @IBOutlet weak var hideRed: UIButton!
+    @IBOutlet weak var hideBlue: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +25,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func redButton(sender: AnyObject) {
+        
+        redColor.hidden = true
+        if blueColor.hidden == true {
+            redColor.hidden = false
+        }
+    }
 
+    @IBAction func blueButton(sender: AnyObject) {
+        
+        blueColor.hidden = true
+        
+        
+    }
 
+    
 }
 
